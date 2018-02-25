a0b486a8a5e573c9230f95bc6f1ef7a8333e88cb
Milinda Bandara <dev.milinda@gmail.com>
2018-02-25 23:53:28 +0530

    modified css for login page

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     'connection': {
         'host': 'localhost',
         'user': 'root',
-        'password': 'mastermind92'
+        'password': 'root'
     },
 	'database': 'my_schema',
     'users_table': 'users'
