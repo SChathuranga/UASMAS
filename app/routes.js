7584ef178de642eb622ee6e05e19067a9440dec7
Milinda Bandara <dev.milinda@gmail.com>
2018-03-11 11:37:24 +0530

    add HomePage for the App and CSS works

--- app/routes.js
+++ app/routes.js
@@ -20,7 +20,7 @@ module.exports = function(app, passport) {
 
 	// process the login form
 	app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
+            successRedirect : '/homepage', // redirect to the secure profile section
             failureRedirect : '/login', // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages
 		}),
@@ -58,6 +58,17 @@ module.exports = function(app, passport) {
 	// we will use route middleware to verify this (the isLoggedIn function)
 	app.get('/profile', isLoggedIn, function(req, res) {
 		res.render('profile.ejs', {
+			user : req.user // get the user out of session and pass to template
+		});
+	});
+
+	// =====================================
+	// HOMEPAGE SECTION =========================
+	// =====================================
+	// we will want this protected so you have to be logged in to visit
+	// we will use route middleware to verify this (the isLoggedIn function)
+	app.get('/homepage', isLoggedIn, function(req, res) {
+		res.render('homepage.ejs', {
 			user : req.user // get the user out of session and pass to template
 		});
 	});
